0a86b677a17e73a1b14d47c44a17acd5945c8f5c
Guillermo Rauch <rauchg@gmail.com>
2013-11-21 19:30:07 -0300

    index: fix tailing and buffer limits

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var carry = require('carrier').carry;
 var request = require('superagent');
 var program = require('commander');
+var tail = require('tailfd').tail;
 var debug = require('debug')('cloudup:log-reader');
 
 /**
@@ -14,7 +15,7 @@ var debug = require('debug')('cloudup:log-reader');
  */
 
 program
-.option('-m, --max <events>', 'max number of events to buffer [20000]')
+.option('-m, --max <max>', 'max buffer size in bytes')
 .option('-f, --file <file>', 'file to read events from')
 .option('-i, --input <key>', 'loggly input key (required)')
 .option('-t, --tag <tag>', 'comma-separated tags to apply')
@@ -29,6 +30,12 @@ if (!program.input) {
   program.outputHelp();
   process.exit(1);
 }
+
+/**
+ * Max buffer size (5mb).
+ */
+
+var max = program.max || 5242880;
 
 /**
  * Loggly POST url.
@@ -47,10 +54,12 @@ if (program.tag) {
 var stream;
 
 if (!program.file) {
+  debug('read stdin');
   process.stdin.resume();
-  stream = process.stdin;
+  carry(process.stdin, online);
 } else {
-  stream = fs.createReadStream(program.file);
+  debug('read file "%s"', program.file);
+  tail(program.file, online);
 }
 
 /**
@@ -60,22 +69,25 @@ if (!program.file) {
 var buffer = '';
 var req;
 
-carry(stream, function(data){
-  if (buffer.length == program.max) {
-    console.error('WARN: discarding event, max buffer size reached');
-    return;
-  }
-
+function online(data){
   if (!~data.indexOf('Event:')) {
     debug('ignoring line');
     return;
   }
 
-  debug('buffering event');
+  // buffer.length + comma + json object minus prefix
+  // cant exceed 5mb buffer
+  var len = buffer.length + 1 + data.length - 7;
+  if (len >= max) {
+    console.error('WARN: discarding event, max buffer size reached');
+    return;
+  }
+
+  debug('buffering event (size at %d)', len);
   if (buffer.length) buffer += ',';
   buffer += data.substr(7);
   flush();
-});
+}
 
 function flush(){
   if (req) return;
@@ -88,7 +100,10 @@ function flush(){
   .type('json')
   .send('[' + buffer + ']')
   .end(function(err, res){
-    if (err || res.error) { console.error(res.text); throw err; }
+    if (err || res.error) {
+      if (res && res.text) console.error(res.statusCode, res.text);
+      throw err;
+    }
     debug('flushed %d events', n);
     req = null;
     process.nextTick(flush);
